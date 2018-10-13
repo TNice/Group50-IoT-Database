07913b1a84f38ae19e97824ad57021de712e1dfc
TNice <tylernicewarner@yahoo.com>
2018-10-13 18:05:15 -0500

    Home Page
    Finished template of home page

--- home.php
+++ home.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <!--Required Meta-->
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <!--Bootstrap-->
@@ -13,9 +12,116 @@
         <script src="main.js"></script>
     </head>
         
-    <body>        
-        <?php include 'navmenu.php'; ?> <!-- Adds Navigation Menu. Able to change based on login status or permissions -->
-        <div>
+    <body>
+        <div class="background">  
+            <div id='title' class='container-fluid titleBox'>            
+                <h1 class='title1'>IoT Database Project</h1>            
+            </div>   
+            <?php include 'navmenu.php'; ?>
+            <div id='info' class="row">
+                <div class='col-2'>
+                    <div class='list-group list-group-flush contentBox2 sticky-top stickyOffset'>
+                        <li class='list-group-item homeTitle' style='font-weight:bold;color:lightgrey;'>Packages</li>
+                        <a href='#package0' class='list-group-item homeList' style='color:lightgrey;'>Package0</a>
+                        <a href='#package1' class='list-group-item homeList' style='color:lightgrey;'>Package1</a>
+                        <a href='#package2' class='list-group-item homeList' style='color:lightgrey;'>Package2</a>
+                        <li class='list-group-item homeTitle' style='font-weight:bold;color:lightgrey;'>Devices</li>
+                        <a href='#device0' class='list-group-item homeList' style='color:lightgrey;'>Device0</a>
+                        <a href='#device1' class='list-group-item homeList' style='color:lightgrey;'>Device1</a>
+                        <a href='#device2' class='list-group-item homeList' style='color:lightgrey;'>Device2</a>
+                    </div>
+                </div>    
+                <div class='col-10'>
+                    <br>
+                    <div>
+                        <h1 class='title2'>Packages</h1>
+                    </div>   
+                    <br>
+                    <div id='package0' class='row'></div>
+                    <div  class="contentBox row">                   
+                        <div class='col-8'>
+                            <h5 class='title1'>Package 0</h5>
+                            <br>
+                            <p class='homeInfo'>This is the package info</p>
+                        </div>
+                        <div class='col-4' style='color:white'>
+                            <h5 class='title1'>Devices</h5>
+                            <br>
+                            <ul>
+                                <li>Device1</li>
+                                <li>Device2</li>
+                            </ul>
+                        </div>  
+                    </div>
+                    <br>
+                    <div id='package1' class='row'></div>
+                    <div  class="contentBox row">                   
+                        <div class='col-8'>
+                            <h5 class='title1'>Package 1</h5>
+                            <br>
+                            <p class='homeInfo'>This is the package info</p>
+                        </div>
+                        <div class='col-4' style='color:white'>
+                            <h5 class='title1'>Devices</h5>
+                            <br>
+                            <ul>
+                                <li>Device1</li>
+                                <li>Device2</li>
+                            </ul>
+                        </div>  
+                    </div>
+                    <br>
+                    <div id='package2' class='row'></div>
+                    <div  class="contentBox row">                   
+                        <div class='col-8'>
+                            <h5 class='title1'>Package 2</h5>
+                            <br>
+                            <p class='homeInfo'>This is the package info</p>
+                        </div>
+                        <div class='col-4' style='color:white'>
+                            <h5 class='title1'>Devices</h5>
+                            <br>
+                            <ul>
+                                <li>Device1</li>
+                                <li>Device2</li>
+                            </ul>
+                        </div>  
+                    </div>
+                    <div>
+                        <h1 class='title2'>Devices</h1>
+                    </div>   
+                    <br>
+                    <div id='device0' class='row'></div>
+                    <div  class="contentBox row">                   
+                        <div class='col-12'>
+                            <h5 class='title1'>Device 0</h5>
+                            <br>
+                            <p class='homeInfo'>This is the device info</p>
+                        </div>
+                    </div>
+                    <br>
+                    <div id='device1' class='row'></div>
+                    <div  class="contentBox row">                   
+                        <div class='col-12'>
+                            <h5 class='title1'>Device 1</h5>
+                            <br>
+                            <p class='homeInfo'>This is the device info</p>
+                        </div>
+                    </div>
+                    <br>
+                    <div id='device2' class='row'></div>
+                    <div  class="contentBox row">                   
+                        <div class='col-12'>
+                            <h5 class='title1'>Device 2</h5>
+                            <br>
+                            <p class='homeInfo'>This is the device info</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id='footer' class='container-fluid footerBox'>
+                <h3 class='footer1'>Created By: Group 50</h1>
+            </div>
         </div>
     </body>
 </html>
--- navmenu.php
+++ navmenu.php
@@ -2,9 +2,8 @@
 session_start();
 
 function DeterminLogButtons(){
-    $loginButtons = "<li class='nav-item'><a class='nav-link' href='signup.php'>Sign Up</a></li><li class='nav-link'>|<li><li class='nav-item'><a class='nav-link' href='login.php'>Login</a></li>";
-    $loginActiveButtons = "<li class='nav-item'><a class='nav-link' href='signup.php'>Sign Up</a></li><li class='nav-link'>|<li><li class='nav-item'><a class='nav-link' href='login.php'>Login</a></li>";
-    $signUpActiveButtons = "<li class='nav-item'><a class='nav-link active' href='signup.php'>Sign Up</a></li><li class='nav-link'>|<li><li class='nav-item'><a class='nav-link' href='login.php'>Login</a></li>";
+    $loginButtons = "<li class='nav-item'><a class='nav-link' href='login.php'>Login</a></li>";
+    $loginActiveButtons = "<li class='nav-item'><a class='nav-link active' href='login.php'>Login</a></li>";
     $logoutButtons = "<li class='nav-item'><a class='nav-link' href='logout.php'>Logout</a></li>";
     $currentPage = basename($_SERVER['PHP_SELF']);
     if(!isset($_SESSION['currentUser']) || $_SESSION['currentUser'] == NULL)
@@ -12,9 +11,7 @@ function DeterminLogButtons(){
         if($currentPage == 'login.php'){
             echo $loginActiveButtons;
         }
-        elseif($currentPage == 'signup.php'){
-            echo $signUpActiveButtons;
-        }
+
         else{
             echo $loginButtons;
         }
@@ -34,23 +31,26 @@ function DeterminLogButtons(){
     </ul>
 </div>-->
 
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
   <a class="navbar-brand" href="#">Group50</a>
   <ul class="navbar-nav">
-    <li class="nav-link">|<li>
+    <li class="nav-link">|</li>
     <li class="nav-item">
       <a <?php if(basename($_SERVER['PHP_SELF']) == 'home.php'){echo "class='nav-link active'";} else{echo 'class="nav-link"'; }?> href="#">Home</a>
     </li>
-    <li class="nav-link">|<li>
+    <li class="nav-link">|</li>
     <li class="nav-item">
-      <a class="nav-link" <?php if(basename($_SERVER['PHP_SELF']) == 'er.php'){echo "class='active'";}?> href="er.php">ER</a>
+      <a class="nav-link" <?php if(basename($_SERVER['PHP_SELF']) == 'packages.php'){echo "class='active'";}?> href="er.php">Packages</a>
     </li>
-    <li class="nav-link">|<li>
+    <li class="nav-link">|</li>
     <li class="nav-item">
-      <a class="nav-link" <?php if(basename($_SERVER['PHP_SELF']) == 'tables.php'){echo "class='active'";}?> href="er.php">Tables</a>
+      <a class="nav-link" <?php if(basename($_SERVER['PHP_SELF']) == 'findDevice.php'){echo "class='active'";}?> href="er.php">Find Devices</a>
     </li>
-    <li class="nav-link">|<li>
+    <li class="nav-link">|</li>
+    <li class="nav-item">
+      <a class="nav-link" <?php if(basename($_SERVER['PHP_SELF']) == 'account.php'){echo "class='active'";}?> href="account.php">User Account</a>
+    </li>
+    <li class="nav-link">|</li>
     <?php DeterminLogButtons();?>
   </ul>
 </nav>
-
