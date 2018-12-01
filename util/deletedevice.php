acfbb34b45de062f5a752d94f3dd6ea75cbcd846
hamatiq <44220880+hamatiq@users.noreply.github.com>
2018-12-01 17:08:50 -0600

    delete device done

--- util/deletedevice.php
+++ util/deletedevice.php
@@ -1,26 +1,43 @@
 <?php
 include 'sqlFunctions.php';
+
 $id = $_REQUEST['id'];
 $type = $_REQUEST['type'];
 
+$query = "DELETE FROM package_device WHERE deviceId = {$id};";
+
+
+SqlQueryRaw($query);
+
+$query = 'no type';
 switch ($type){
     case 'plug':
         $query = "DELETE FROM smartplug WHERE id = {$id}";
+        
         SqlQueryRaw($query);
         break;
     case 'wifi':
         $query = "DELETE FROM wifi WHERE id = {$id}";
+        
         SqlQueryRaw($query);
         break;
+
     case 'print':
         $query = "DELETE FROM printer WHERE id = {$id}";
+        
         SqlQueryRaw($query);
         break;
     default:
         break;
 }
 
+
+
+
+
 $query = "DELETE FROM devices WHERE id = {$id}";
+
+
 SqlQueryRaw($query);
 
 ?>
