3d7621abf00cb0e3b85e2e0f39d14c76a4e241b6
TNice <tylernicewarner@yahoo.com>
2018-12-01 12:30:19 -0600

    delete obsolete modal

--- admin.php
+++ admin.php
@@ -734,65 +734,6 @@
                     
                 </div>   
             </div>    
-            <div class='modal' id='addDeviceModal' style='visibility:hidden;'>
-                <div class='modal-dialog modal-lg' style='z-index:10'>
-                    <div class='modal-content' id='userEdit'>
-                    <div class='modal-header'>
-                        <h4 class='modal-title' id='idModal' style='margin:auto;width:100%;text-align:center'>"."{$row['id']}"."</h4>
-                        <button type='button' class='close' onclick='CloseModal(0);'>×</button>
-                    </div>
-                    <div class='modal-body'>
-                        <div class='row'>
-                            <div class='col-1'></div>
-                            <div class='col-5'>
-                                <div class='input-group mb-3'>
-                                    <div class='input-group-prepend'>
-                                        <span class='input-group-text'>Name</span>
-                                    </div>
-                                    <input type='text' class='form-control' placeholder='firstName' id='fnameModal'>
-                                    <input type='text' class='form-control' placeholder='lastName' id='lnameModal'> 
-                                </div>
-                                <div class='input-group mb-3'>
-                                    <div class='input-group-prepend'>
-                                        <span class='input-group-text'>Username</span>
-                                    </div>
-                                <input type='text' class='form-control' placeholder='userName' id='usernameModal'>
-                                </div>
-                                <div class='input-group mb-3'>
-                                    <div class='input-group-prepend'>
-                                        <span class='input-group-text'>Email</span>
-                                    </div>
-                                <input type='text' class='form-control' placeholder='email' id='emailModal'>
-                                </div>
-                            </div>
-                            <div class='col-5'>
-                                <div class='input-group mb-3'>
-                                    <div class='input-group-prepend'>
-                                        <span class='input-group-text'>Phone#</span>
-                                    </div>
-                                    <input type='text' class='form-control' placeholder='555-555-5555' id='phoneModal'>
-                                </div>
-                                <div class='input-group mb-3'>
-                                    <div class='input-group-prepend'>
-                                        <span class='input-group-text'>Birth Date</span>
-                                    </div>
-                                <input type='date' class='form-control' id='bdayModal'>
-                                </div>
-                            </div>
-                            <div class='col-1'></div>
-                        </div>
-                    </div>
-                    <div class='modal-footer'>
-                        <form style='width:100%'>
-                            <button class='btn btn-secondary' id='editButton' onclick='EditModal(event);return false;' style='text-align:left;'>Edit</button>
-                            <button class='btn btn-secondary' id='saveButton' onclick='SaveUserModal(event);return false;' style='display:none'>Save</button>
-                            <button class='btn btn-secondary' id='cancelButton' onclick='CancelEdit(event);return false;' style='display:none'>Cancel</button>
-                            <button class='btn btn-danger' type='submit' name='deleteAcct' style='float:right'>DELETE</button>
-                        </form>
-                    </div>
-                    </div>
-                </div>
-            </div>
         </div> 
         <div id='Modal' style='visibility:hidden;'>
             <div class='modal-dialog modal-lg' style='z-index:10'>
