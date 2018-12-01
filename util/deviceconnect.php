c23db03750d832c1cbc655037470e1d53288e752
Austin Hales <pjh539@mail.missouri.edu>
2018-12-01 16:33:28 -0600

    added date check for connect button

--- util/deviceconnect.php
+++ util/deviceconnect.php
@@ -3,10 +3,49 @@
     include 'sqlFunctions.php';
     $id = $_REQUEST['id'];
     $query = "select roleId from user_role where userId = {$_SESSION['currentUser']};";
+//    echo "{$_SESSION['currentUser']} ";
     
     $row = SqlQuery($query);
 
-     echo "select * from access_rule a, user_role u where a.roleId = {$row['roleId']} and a.deviceId = {$id}"
-    
+    $query = "select * from access_rule where roleId = {$row['roleId']} and deviceId = {$id}";
 
+    $row = SqlQuery($query);
+    
+//$time = $row[''];
+$ruleId = $row['ruleId'];
+$query = "SELECT * FROM accesstime_rule WHERE ruleId = {$ruleId};";
+$result = SqlQueryRaw($query);
+
+$today = date("l"); // = current day of week
+$today = strtolower($today);
+$currentTime; // = current time
+//echo $today;
+
+$response = false;
+
+echo " {$query} ";
+//echo "{$row['accessDate']} date ";
+while($row = mysqli_fetch_assoc($result)){
+    echo $today;
+    echo $row['accessDate'];
+    if(strcmp($row['accessDate'], "any")){
+        $response = true;
+        break;
+    }
+    if(strcmp($row['accessDate'], $today)){
+        $response = true;
+        break;
+    }
+    echo "hello";
+    echo $row['accessDate'];
+    
+}
+
+if($response == true){
+    //check time
+    echo "TRUE";
+}
+else{
+    echo "False";
+}
 ?>
