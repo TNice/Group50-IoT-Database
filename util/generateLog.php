6e28d768ce05e3f31ae9083b020101eb0694c97b
Austin Hales <pjh539@mail.missouri.edu>
2018-12-02 14:28:10 -0600

    finished function to add log when device is connected

--- util/generateLog.php
+++ util/generateLog.php
@@ -1,14 +1,54 @@
 <?php
+include 'sqlFunctions.php';
+
+function GenerateLogId(){
+    srand(make_seed());
+    $temp = rand(10000, 99999);
+    if(CheckForDuplicateLogId($temp) === TRUE){
+        return GenerateLogId();
+    }
+    else{
+        echo $temp;
+        return $temp;
+    }
+}
 
 
+function make_seed()
+{
+  list($usec, $sec) = explode(' ', microtime());
+  return $sec + $usec * 1000000;
+}
+
+function CheckForDuplicateLogId($logId){
+   // $connection = new mysqli($GLOBALS['server'], $GLOBALS['user'], $GLOBALS['pass'], $GLOBALS['db']);
+    $sqlQuery = 'SELECT logId FROM deviceLogs';
+    //$result = $connection->query($sqlQuery);
+    $result = SqlQueryRaw($sqlQuery);
+        while($row = mysqli_fetch_assoc($result)){
+            if($row['logId'] == $logId){
+                return TRUE;
+            }
+        }
+    return FALSE;
+}
+
+session_start();
+$userId =  $_SESSION['currentUser'];
 
 $deviceId = $_REQUEST['deviceId'];
-$userId = $_REQUEST['userId'];
+$connectResult = $_REQUEST['connectResult'];
+
+$logId = GenerateLogId();
 //generate log Id
 //time
- $currentTime = date("h:i:sa");
+$currentTime = time();
 
-$query = "INSERT INTO deviceLogs values({$deviceId}, {$userId}, {$logId}, {$currentTime})"
+$query = "INSERT INTO deviceLogs values({$deviceId}, {$userId}, {$logId}, {$connectResult}, {$currentTime});";
+    
+$row = SqlQuery($query); 
+    
+    
 
 
 ?>
