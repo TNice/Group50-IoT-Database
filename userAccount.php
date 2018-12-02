3b935d6aadcfa9884cffff12915fcacc431fba5d
TNice <tylernicewarner@yahoo.com>
2018-12-02 15:48:37 -0600

    attempt to fix error

--- userAccount.php
+++ userAccount.php
@@ -91,6 +91,41 @@
             unset($_POST['package']);
        }
    }
+
+    function GenerateDeviceList($id){
+        $list = "<ul>";
+
+        $query = "SELECT * FROM package_device WHERE packageId = {$id}";
+        $result = SqlQueryRaw($query);
+
+        while($row = mysqli_fetch_assoc($result)){
+            $newQuery = "SELECT * FROM smartplug WHERE id = {$row['deviceId']}";
+            $newRow = SqlQuery($newQuery);
+            if(isset($newRow['deviceId'])){
+                $list .= "<li>Smart Plug</li>"
+            }
+            else{
+                $newQuery = "SELECT * FROM printer WHERE id = {$row['deviceId']}";
+                $newRow = SqlQuery($newQuery);
+                if(isset($newRow['deviceId'])){
+                    $list .= "<li>Printer</li>"
+                }
+                else{
+                    $newQuery = "SELECT * FROM wifi WHERE id = {$row['deviceId']}";
+                    $newRow = SqlQuery($newQuery);
+                    if(isset($newRow['deviceId'])){
+                        $list .= "<li>Wifi</li>"
+                    }
+                    else{
+                        $list .= "<li>Other</li>"
+                    }
+                }
+            }
+        }
+
+        $list .= "</ul>";
+        return $list;
+    }
 ?>
 
 <html>
@@ -244,16 +279,16 @@
                                             GetPackageInfo($GLOBALS['package'])
                                             ."
                                         </div>
-                                        <div class='col-6' id='deviceList'>
-                                           
-                                        </div>
+                                        <div class='col-6'>" .
+                                           GenerateDeviceList($GLOBALS['package'])
+                                        . "</div>
                                     </div>
                                 </div>";
                                 echo $element;
                             }
                             //If user is subscribed to package add a current package tab
                         ?>
-                        <script>
+                      <!--  <script>
                             function loadDeviceList(id){
                                 var xmlhttp = new XMLHttpRequest();
                                 xmlhttp.onreadystatechange = function() {
@@ -265,8 +300,8 @@
                                 xmlhttp.send();
                             }
 
-                            loadDeviceList("<?php echo $GLOBALS['package'];?>");
-                        </script>
+                            loadDeviceList("");
+                        </script> -->
                         <div class='contentBox'>
                             <h3 class='title2' style='margin-bottom:1.5rem;'>Subscribe To Package</h3>
                             <!--Radio button values for packages must be the corisponding package id-->
